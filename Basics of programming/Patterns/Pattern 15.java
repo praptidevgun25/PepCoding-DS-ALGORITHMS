f3b906d5499be5900320ed17bedc38982050ea22
Prapti Devgun <75064110+praptidevgun25@users.noreply.github.com>
2020-12-05 20:45:15 +0530

    Create Pattern 15.java

---
+++ Basics of programming/Patterns/Pattern 15.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n=scn.nextInt();
+        int sp = n/2;
+        int st = 1;
+        int val=1;
+        for(int i=1;i<=n;i++) {
+        for(int j=1;j<=sp;j++) {
+            System.out.print("\t");
+        }
+        int cval=val;
+        for(int j=1;j<=st;j++) {
+            System.out.print(cval + "\t");
+            if(j<=st/2) {
+                cval++;
+            } else {
+                cval--;
+            }
+        }
+        
+        if(i<=n/2) {
+            sp--;
+            st+=2;
+            val++;
+        } else {
+            sp++;
+            st-=2;
+            val--;
+        }
+        System.out.println();
+        
+
+    }
+}
+}
